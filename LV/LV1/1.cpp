a700356ccbe95abc71a93def1669f61a4c3237bf
d4nin <daninsadzak@gmail.com>
2026-04-05 12:20:34 +0200

    LV - LV1 - Prvi Zadatak

---
+++ LV/LV1/1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+/*
+    Napisati program koji ispisuje u konzolu tekst:
+    Ispisujem tekst
+    u dva reda
+*/
+
+int main() {
+    std::cout << "Ispisujem tekst\n" << "  u dva reda\n";
+    return 0;
+}
