021ab93fc9095e88f552e5d4f77c12c58d13ede1
d4nin <daninsadzak@gmail.com>
2026-04-19 14:19:56 +0200

    LV - LV7 - Zadatak 2 d)

--- LV/LV7/2.cpp
+++ LV/LV7/2.cpp
@@ -15,25 +15,36 @@
     b) Potom napravite primjer sa vektorom pokazivaca na stringove.
 
     c) Zatim koristite labda funkciju za drugi parametar.
+
+    d) Na kraju, napravite puni primjer: unos stringova dok se ne dodje do rijeci "KRAJ",
+       zatim dodatni parametar - string - koji oznacava rastuci ili opadajuci sort 
+       ("R" ili "O"), i potom se vrst sortiranje shodno zahtijevanoj vrsti. Ispisite
+       rezultat.
 */
 
 template <typename T>
-std::list<T> sortiraj(std::vector<T> v, bool (*kriterij)(T, T)) {
+std::list<T> sortiraj(std::vector<T> v, bool (*kriterij)(T, T), std::string sortiranje) {
     std::list<T> nova_lista(v.begin(), v.end());
-    nova_lista.sort(kriterij);
+    if (sortiranje == "O") nova_lista.sort(kriterij);
+    if (sortiranje == "R") nova_lista.sort([](std::string* a, std::string* b) {
+        return *a > *b;
+    });
     return nova_lista;
 }
 
 int main() {
     std::vector<std::string*> v;
-    v.push_back(new std::string("1234"));
-    v.push_back(new std::string("Ananas"));
-    v.push_back(new std::string("abcd"));
-    v.push_back(new std::string("jabuka"));
+    std::string rijec;
+    std::cout << "Unos rijeci:\n";
+    while (true) {
+        std::getline(std::cin, rijec);
+        if (rijec == "KRAJ") break;
+        v.push_back(new std::string(rijec));
+    }
     std::list<std::string*> l = sortiraj<std::string*>(v, [](std::string* a, std::string* b) {
         if (a->length() == b->length()) return *a < *b;
         return a->length() < b->length();
-    });
+    }, "R");
     for (const auto& lista : l) std::cout << *lista << " ";
     std::cout << std::endl;
 
