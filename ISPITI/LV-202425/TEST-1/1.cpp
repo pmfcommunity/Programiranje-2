bf219b1bbfe6980e4d71f6e8b8eaeff9741330c9
d4nin <daninsadzak@gmail.com>
2026-04-19 17:59:00 +0200

    ISPITI - LV-2024/25 - TEST-1 - Zadatak 1

---
+++ ISPITI/LV-202425/TEST-1/1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+/*
+    Napisati funkciju koja prima dva vektora stringova v1 i v2. Funkcija treba da ukloni
+    sve razmake iz stringova u prvom vektoru, te da rezultujuce stringove smjesti u 
+    drugi vektor. Npr. ako je v1 = {"a b c", "123 456  ", "  123"}, nakon pozivanja
+    funkcije, vektor v2 postaje: v2 = {"abc", "123456", "123"}. Ako je vektor v1 prazan,
+    funkcija baza izuzetak. Napisati kratki testni program. Zadatak uraditi bez koristenja
+    ugradjenih funkcija za stringove.
+*/
+
+void ukloni_razmake(const std::vector<std::string>& v1, std::vector<std::string>& v2) {
+    for (int i = 0; i < v1.size(); i++) {
+        std::string rijec = "";
+        std::string trenutna_rijec = v1.at(i);
+        for (int j = 0; j < trenutna_rijec.length(); j++) {
+            if (trenutna_rijec[j] == ' ') continue;
+            rijec += trenutna_rijec[j];
+        }
+        v2.push_back(rijec);
+    }
+}
+
+int main() {
+    std::vector<std::string> v1 = {"a b c", "123 456  ", "  123"}, v2;
+    ukloni_razmake(v1, v2);
+    std::cout << "Vektor sa uklonjenim razmacima:\n";
+    for (std::string v : v2) std::cout << v << " ";
+    std::cout << std::endl;
+    return 0;
+}
